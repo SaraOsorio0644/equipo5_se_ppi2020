9365568fbd815074018b187bbc3f6a0e0cee0bf5
Sara Yulieth Osorio Hincapie <66147792+SaraYuliethOsorioHincapie@users.noreply.github.com>
2020-11-03 21:28:07 -0500

    initial commit

--- frontend/src/componentes/Finalpagina/Final_Inicioadm.js
+++ frontend/src/componentes/Finalpagina/Final_Inicioadm.js
@@ -1,23 +1,17 @@
 import React from "react";
-import "../../styles.css"; //Está ruta la corregí 
+import "../../styles.css"; //Está ruta la corregí
 import logo from "../../images/logooo.jpg";
 
 function Final_Inicioadm() {
   return (
     <div>
       <p> </p>
-       <div className="logo">
+      <div className="logo">
         <img src={logo} />
       </div>
-      <p className="Final_inicio">5382978 ò 3013134050
-        
-   
-     </p>
-     <p className="Final_inicio2">@ColectivosSantaElena 
-        
-   
-     </p>
-        </div>
-  )
+      <p className="Final_inicio">5382978 ò 3013134050</p>
+      <p className="Final_inicio2">@ColectivosSantaElena</p>
+    </div>
+  );
 }
 export default Final_Inicioadm;
