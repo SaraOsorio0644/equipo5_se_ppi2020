3971c8790705d87a1a413c2149ea9e61884790ea
Sara Yulieth Osorio Hincapie <66147792+SaraYuliethOsorioHincapie@users.noreply.github.com>
2020-11-03 21:55:21 -0500

    ENLACE IMAGENES

--- frontend/src/componentes/Descripcion/D_Inicioadm.js
+++ frontend/src/componentes/Descripcion/D_Inicioadm.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Inicio from "../../images/Inicio.jpg"; //Esta ruta la corregí 
+
 import "../../styles.css";
 function D_Inicioadm() {
   return(
     <div className="Imagen1">
       
-  <img className = "Imagenprincipal" src={Inicio}/>; 
+  <img className = "Imagenprincipal" src="https://lh3.googleusercontent.com/proxy/BaMU70-KznSqOmkEY1EzjCuokbnNbkPsRiFjuxo8v3_oq-6TXsPi7K64Sz7cuHflxH6Md5tj7LvZttsrkZtlpwArim3PiAhJ7RWrx8vBqDQH_EH88V1a0nvDuW3ZXksykMHS7VBvM_BoBw"/>; 
 </div>
   )
   }
