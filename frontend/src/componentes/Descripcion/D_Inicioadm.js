c060561ba52971bed3a538a3c70a2c7a2ee51fd0
Sara Yulieth Osorio Hincapie <66147792+SaraYuliethOsorioHincapie@users.noreply.github.com>
2020-11-05 11:52:01 -0500

    Corrección Imagen Inicio

--- frontend/src/componentes/Descripcion/D_Inicioadm.js
+++ frontend/src/componentes/Descripcion/D_Inicioadm.js
@@ -5,7 +5,7 @@ function D_Inicioadm() {
   return(
     <div className="Imagen1">
       
-  <img className = "Imagenprincipal" src="https://lh3.googleusercontent.com/proxy/BaMU70-KznSqOmkEY1EzjCuokbnNbkPsRiFjuxo8v3_oq-6TXsPi7K64Sz7cuHflxH6Md5tj7LvZttsrkZtlpwArim3PiAhJ7RWrx8vBqDQH_EH88V1a0nvDuW3ZXksykMHS7VBvM_BoBw"/>; 
+  <img className = "Imagenprincipal" src="https://davidlmedina.files.wordpress.com/2013/04/copia-santa-elena-1-187.jpg?w=1200"/>; 
 </div>
   )
   }
--- frontend/src/componentes/Descripcion/D_valoracion.js
+++ frontend/src/componentes/Descripcion/D_valoracion.js
@@ -12,7 +12,7 @@ function D_Valoracion() {
 <button type="button" className="btn btn-warning">5</button>
 </div>
 <div className="Imagen1">
-<img src="https://lh3.googleusercontent.com/proxy/BaMU70-KznSqOmkEY1EzjCuokbnNbkPsRiFjuxo8v3_oq-6TXsPi7K64Sz7cuHflxH6Md5tj7LvZttsrkZtlpwArim3PiAhJ7RWrx8vBqDQH_EH88V1a0nvDuW3ZXksykMHS7VBvM_BoBw" />
+<img src="https://davidlmedina.files.wordpress.com/2013/04/copia-santa-elena-1-187.jpg?w=1200" />
 </div>
 </>
   );
